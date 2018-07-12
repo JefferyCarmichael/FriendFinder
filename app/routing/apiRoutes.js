21e64401f8c03854031b812b3151311e5118ffa2
Jeff2C <jeff.c.carmichael@gmail.com>
2018-07-11 20:17:51 -0400

    initial

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,38 @@
+var path 			= require("path");
+var friends = require("../data/friends");
+
+
+// ROUTING
+
+module.exports = function(app) {
+
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+
+  // app.post("/api/tables", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body-parser middleware
+    // if (tableData.length < 5) {
+  //     tableData.push(req.body);
+  //     res.json(true);
+  //   }
+  //   else {
+  //     waitListData.push(req.body);
+  //     res.json(false);
+  //   }
+  // });
+
+  // ------------------------
+
+  app.post("/api/clear", function() {
+    // Empty out the arrays of data
+    tableData = [];
+    waitListData = [];
+
+    console.log(tableData);
+  });
+};
---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,21 @@
+// =========================
+var path = require("path");
+
+// ROUTING
+
+module.exports = function(app) {
+  //GET Requests
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
+
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+  });
+
+  // Default
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
+};
